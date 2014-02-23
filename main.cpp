b98574cf719dc1357c3a2c07a76f1ee5e89e4255
smashmx <smashdevmx@gmail.com>
2014-02-23 00:11:12 -0700

    All Unit tests completed
    - Improvements in assertEqual function
    - Added all set of Unit tests
    - Bugs fixed (in MAX_THROWS and MAX_FRAMES

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-//#include <string>
 
 class BowlingScore {
 	private:
-		const int MAX_THROWS = 20; // 21 throws (base 0)
-		const int MAX_FRAMES = 9; // 10 frames (base 0)
+		const int MAX_THROWS = 21;
+		const int MAX_FRAMES = 10;
 		int throws[21] = {};
 		int current_throw = 0;
 
@@ -28,21 +27,48 @@ class BowlingScore {
 			addThrow(5);
 		}
 
+		void addNone() {
+			addThrow(0);
+			addThrow(0);
+		}
+
 		void calculateScore() {
 			for (int current_frame = 0; current_frame < MAX_FRAMES; ++current_frame) {
-				int starting_frame = current_frame * 2;
-				int throw_1 = throws[starting_frame];
-				int throw_2 = throws[starting_frame + 1];
+				int starting_throw = current_frame * 2;
+				int throw_1 = throws[starting_throw];
+				int throw_2 = throws[starting_throw + 1];
 				int current_throws = throw_1 + throw_2;
+				int next_throw = starting_throw + 2;
+
 				if (throw_1 == 10) {
 					// Calculate strike
 					score_frames[current_frame] = current_throws;
+
+					// Calculate the next throws to be added as a bonus
+					if (current_frame == 9) { // Frame 10
+						score_frames[current_frame] += throws[starting_throw + 2];
+					} else { // Any other frame
+						int next_throws_1 = throws[next_throw];
+						int second_bonus_position = 0;
+
+						if (next_throws_1 == 10) {
+							// If we have a strike, ignore the second throw and take
+							// into account the next throw
+							second_bonus_position = starting_throw + 4;
+						} else {
+							// If we have not a strike, count the second throw of the frame
+							second_bonus_position = starting_throw + 3;
+						}
+
+						int next_throws_2 = throws[second_bonus_position];
+
+						score_frames[current_frame] += next_throws_1 + next_throws_2;
+					}
 				} else if ((throw_1 + throw_2) == 10) {
 					// Calculate spare (Next throw)
 					score_frames[current_frame] = current_throws;
 
 					// Add the next if it's available
-					int next_throw = starting_frame + 2;
 					if (next_throw < MAX_THROWS) {
 						score_frames[current_frame] += throws[next_throw];
 					}
@@ -60,14 +86,22 @@ class BowlingScore {
 		int getFinalScore() {
 			return final_score;
 		}
+
+		void printScores() {
+			std::cout << std::endl;
+			for (int current_frame = 0; current_frame < MAX_FRAMES; ++current_frame) {
+				std::cout << current_frame << ": " << score_frames[current_frame];
+				std::cout << std::endl;
+			}
+		}
 };
 
 void assertEqual(int var_a, int var_b, char* func_name) {
 	if (var_a != var_b) {
-		std::cout << "F" << " " << var_a << " not equal to " << var_b << std::endl;
-		std::cout << func_name << std::endl;
+		std::cout << std::endl << "F " << func_name << " " << var_a << " not equal to " << var_b;
+		std::cout << std::endl;
 	} else {
-		std::cout << "." << std::endl;
+		std::cout << ".";
 	}
 }
 
@@ -142,10 +176,10 @@ void test_7() {
 }
 
 void test_8() {
-	// Test basic spares
+	// Test strikes in a row
 	BowlingScore *bs = new BowlingScore();
-	bs->addThrow(10);
-	bs->addThrow(10);
+	bs->addStrike();
+	bs->addStrike();
 	bs->addThrow(4);
 	bs->addThrow(2);
 	bs->calculateScore();
@@ -153,17 +187,51 @@ void test_8() {
 	assertEqual(bs->getFinalScore(), 46, (char*)__FUNCTION__);
 }
 
+void test_9() {
+	// Test strikes and some other thows
+	BowlingScore *bs = new BowlingScore();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addThrow(4);
+	bs->addThrow(6);
+	bs->addThrow(2);
+	bs->addThrow(0);
+	bs->calculateScore();
+
+	assertEqual(bs->getFinalScore(), 58, (char*)__FUNCTION__);
+}
+
+void test_10() {
+	// Test the last 3 frames
+	BowlingScore *bs = new BowlingScore();
+	bs->addNone();
+	bs->addNone();
+	bs->addNone();
+	bs->addNone();
+	bs->addNone();
+	bs->addNone();
+	bs->addNone();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addThrow(10);
+	bs->addThrow(10);
+	bs->addThrow(10);
+	bs->calculateScore();
+
+	assertEqual(bs->getFinalScore(), 90, (char*)__FUNCTION__);
+}
+
 void test_perfect_game() {
 	BowlingScore *bs = new BowlingScore();
-	bs->addSpare();
-	bs->addSpare();
-	bs->addSpare();
-	bs->addSpare();
-	bs->addSpare();
-	bs->addSpare();
-	bs->addSpare();
-	bs->addSpare();
-	bs->addSpare();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addStrike();
+	bs->addStrike();
 	bs->addThrow(10);
 	bs->addThrow(10);
 	bs->addThrow(10);
@@ -171,6 +239,26 @@ void test_perfect_game() {
 	bs->calculateScore();
 
 	assertEqual(bs->getFinalScore(), 300, (char*)__FUNCTION__);
+}
+
+void test_dutch200() {
+	BowlingScore *bs = new BowlingScore();
+	bs->addSpare();
+	bs->addStrike();
+	bs->addSpare();
+	bs->addStrike();
+	bs->addSpare();
+	bs->addStrike();
+	bs->addSpare();
+	bs->addStrike();
+	bs->addSpare();
+	bs->addThrow(10);
+	bs->addThrow(0);
+	bs->addThrow(10);
+
+	bs->calculateScore();
+
+	assertEqual(bs->getFinalScore(), 200, (char*)__FUNCTION__);
 }
 
 void test_suite() {
@@ -182,7 +270,10 @@ void test_suite() {
     test_6();
     test_7();
     test_8();
+    test_9();
+    test_10();
     test_perfect_game();
+    test_dutch200();
 }
 
 int main() {
